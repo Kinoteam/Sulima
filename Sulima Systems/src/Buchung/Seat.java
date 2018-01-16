59737e191797391165999571eeb51c2c534f86ea
Abrahalhabachi <33718367+Abrahalhabachi@users.noreply.github.com>
2018-01-16 10:12:42 +0100

    Some Classes

---
+++ Sulima Systems/src/Buchung/Seat.java
@@ -0,0 +1,55 @@
+package Buchung;
+
+public class Seat {
+	private int seatNumber;
+	private boolean reserved;
+	private String firstName;
+	private String lastName;
+	
+	public Seat() {
+		seatNumber = 0;
+		reserved = false;
+		firstName= "";
+		lastName= "";
+	}
+	
+	private Seat(int pNumber, boolean pReserved, String pFirstName, String pLastName) {
+		seatNumber = pNumber;
+		reserved = pReserved;
+		firstName= pFirstName;
+		lastName= pLastName;
+	}
+	
+	public void setResetSeat(int pNumber) {
+		seatNumber = pNumber;
+		reserved = false;
+		firstName= "";
+		lastName= "";
+	}
+	
+	public void setReservedSeat(String pFirstName, String pLastName) {
+		reserved = true;
+		firstName= pFirstName;
+		lastName= pLastName;
+	}
+	
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+	
+	public boolean getReserved() {
+		return reserved;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+}
+
+
+
---
+++ Sulima Systems/src/Buchung/TestSeat.java
@@ -0,0 +1,25 @@
+package Buchung;
+
+import java.util.*;
+public class TestSeat {
+
+	public static void main(String[] args) {
+		
+		int totalSeats = 10;
+		Seat theatre[] = new Seat[totalSeats];
+
+		for (int i=0; i<10; i++) {
+			theatre[i] = new Seat();
+			theatre[i].setResetSeat(i+1);
+			System.out.println("Seat created. Nr:" + theatre[i].getSeatNumber() + ". reserved: " + theatre[i].getReserved() + ". name: " + theatre[i].getFirstName() + " " + theatre[i].getLastName());
+		}
+		
+		int seatToModify= 5;
+		theatre[seatToModify+1].setReservedSeat("mamak", "babak");
+		
+		for (int i=0; i<10; i++) {
+			System.out.println("Seat created. Nr:" + theatre[i].getSeatNumber() + ". reserved: " + theatre[i].getReserved() + ". name: " + theatre[i].getFirstName() + " " + theatre[i].getLastName());
+		}
+		
+	}
+}
